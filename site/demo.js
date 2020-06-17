2b16dc0b930c169cb5bc4553523482a4934061b7
Marijn Haverbeke <marijn@haverbeke.nl>
2020-06-17 11:11:16 +0200

    Use basic-setup in demo script

--- site/demo.js
+++ site/demo.js
@@ -1,36 +1,15 @@
-import {EditorState} from "../../state"
-import {EditorView} from "../../view"
-import {keymap} from "../../keymap"
-import {historyKeymap, history} from "../../history"
-import {foldKeymap, foldGutter} from "../../fold"
-import {lineNumbers} from "../../gutter"
-import {defaultKeymap} from "../../commands"
-import {bracketMatching} from "../../matchbrackets"
-import {closeBrackets} from "../../closebrackets"
-import {specialChars} from "../../special-chars"
-import {multipleSelections} from "../../multiple-selections"
-import {searchKeymap} from "../../search"
-import {autocomplete} from "../../autocomplete"
-
-import {javascript} from "../../lang-javascript"
-import {defaultHighlighter} from "../../highlight"
+import {EditorState, EditorView, basicSetup} from "@codemirror/next/basic-setup"
+import {javascript} from "@codemirror/next/lang-javascript"
+import {autocomplete} from "@codemirror/next/autocomplete"
 
 let jsCompletions = "break case catch class const continue debugger default delete do else enum export extends false finally for function if implements import interface in instanceof let new package private protected public return static super switch this throw true try typeof var void while with yield".split(" ")
     .concat(Object.getOwnPropertyNames(window))
 
-let isMac = /Mac/.test(navigator.platform)
 let state = EditorState.create({doc: `function hello(who = "world") {
   console.log(\`Hello, \${who}!\`)
 }`, extensions: [
-  lineNumbers(),
-  history(),
-  specialChars(),
-  foldGutter(),
-  multipleSelections(),
+  ...basicSetup,
   javascript(),
-  defaultHighlighter,
-  bracketMatching(),
-  closeBrackets,
   autocomplete({
     override(state, pos, cx) {
       let prefix = /[\w$]*$/.exec(state.doc.slice(Math.max(0, pos - 30), pos))[0]
@@ -38,14 +17,7 @@ let state = EditorState.create({doc: `function hello(who = "world") {
       return jsCompletions.filter(str => cx.filter(str, prefix))
         .map(str => ({label: str, start: pos - prefix.length, end: pos}))
     }
-  }),
-  keymap([
-    ...defaultKeymap,
-    ...historyKeymap,
-    ...foldKeymap,
-    ...searchKeymap
-  ])
+  })
 ]})
 
-let view = window.view = new EditorView({state})
-document.querySelector("#editor").appendChild(view.dom)
+let view = window.view = new EditorView({state, parent: document.querySelector("#editor")})
