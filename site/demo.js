a7503fbcd5bd6d4b07b67c0eb9daa2db16a29b11
Marijn Haverbeke <marijn@haverbeke.nl>
2020-09-03 10:27:40 +0200

    Richer JS completions in the demo

--- site/demo.js
+++ site/demo.js
@@ -2,7 +2,14 @@ import {EditorState, EditorView, basicSetup} from "@codemirror/next/basic-setup"
 import {javascript, javascriptSyntax} from "@codemirror/next/lang-javascript"
 import {completeFromList} from "@codemirror/next/autocomplete"
 
-let jsCompletion = completeFromList("break case catch class const continue debugger default delete do else enum export extends false finally for function if implements import interface in instanceof let new package private protected public return static super switch this throw true try typeof var void while with yield".split(" ").concat(Object.getOwnPropertyNames(window)))
+let keywords = "break case catch class const continue debugger default delete do else enum export extends false finally for function if implements import interface in instanceof let new package private protected public return static super switch this throw true try typeof var void while with yield".split(" ").map(kw => ({label: kw, type: "keyword"}))
+let globals = Object.getOwnPropertyNames(window).map(p => {
+  return {
+    label: p,
+    type: /^[A-Z]/.test(p) ? "class" : typeof window[p] == "function" ? "function" : "variable"
+  }
+})
+let jsCompletion = completeFromList([...keywords, ...globals])
 
 let state = EditorState.create({doc: `function hello(who = "world") {
   console.log(\`Hello, \${who}!\`)
