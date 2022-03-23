997cee8e8e6aa1e04b638e971f2200c8ffc7fbd4
Alasdair Smith <ali@alasdairsmith.co.uk>
2022-03-23 19:57:46 +0000

    Refactor changelog gen to allow structured data

--- src/changelog.js
+++ src/changelog.js
@@ -2,20 +2,32 @@ const fs = require("fs"), {join} = require("path")
 
 const {core: packages} = require("../../bin/packages")
 
-exports.changelog = function() {
-  let entries = []
-  function extract(log, name) {
-    let release = /(?:^|\n)## ([\d\.]+) \(([\d-]+)\)\n([^]*?)(?=\n## |$)/g
-    for (let m; m = release.exec(log);) {
-      let [, version, date, body] = m
-      entries.push({
-        date,
-        content: (name ? `## [@codemirror/${name}](../ref/#${name})` : `## @codemirror/next`) +
-          ` ${version} (${date})\n${body}`
-      })
+function extract(log, name) {
+  let release = /(?:^|\n)## ([\d\.]+) \(([\d-]+)\)\n([^]*?)(?=\n## |$)/g
+  for (let m; m = release.exec(log);) {
+    let [, version, date, body] = m
+    return {
+      date: new Date(date),
+      name: name ? `@codemirror/${name}` : '@codemirror/next',
+      version,
+      body,
+      content: (name ? `## [@codemirror/${name}](../ref/#${name})` : `## @codemirror/next`) +
+        ` ${version} (${date})\n${body}`
     }
   }
-  for (let name of packages) extract(fs.readFileSync(join(__dirname, "..", "..", name, "CHANGELOG.md"), "utf8"), name)
-  extract(fs.readFileSync(join(__dirname, "historic_changelog.md"), "utf8"))
-  return entries.sort((a, b) => (a.date == b.date ? 0 : a.date < b.date ? 1 : -1)).map(e => e.content).join("\n")
+}
+
+function buildChangelog() {
+  let entries = []
+  for (let name of packages) entries.push(extract(fs.readFileSync(join(__dirname, "..", "..", name, "CHANGELOG.md"), "utf8"), name))
+  entries.push(extract(fs.readFileSync(join(__dirname, "historic_changelog.md"), "utf8")))
+  return entries.sort((a, b) => (a.date == b.date ? 0 : a.date < b.date ? 1 : -1))
+}
+
+exports.changelog = function() {
+  return buildChangelog().map(e => e.content).join("\n")
+}
+
+exports.changelogData = function() {
+  return buildChangelog()
 }
