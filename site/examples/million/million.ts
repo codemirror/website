6349d508677f1451da881541a15c7db49767d984
José dBruxelles <18559798+jdbruxelles@users.noreply.github.com>
2022-06-18 00:32:10 +0200

    Fix logo link

--- site/examples/million/million.ts
+++ site/examples/million/million.ts
@@ -8,7 +8,7 @@ let lines = [`<!doctype html>`, `<meta charset="utf8">`, `<body>`]
 let repeated = [`  <p>These lines are repeated many times to save memory on`,
                 `  string data.</p>`,
                 `  <hr>`,
-                `  <img src="../logo.svg">`,
+                `  <img src="../../style/logo.svg">`,
                 ``]
 for (let i = 0; lines.length < 2e6; i++) lines.push(repeated[i % repeated.length])
 lines.push(`</body>`, ``)
