d5e6adf4bd9e77a368b518294b9ccfa766830e49
Tom Locke <tom@tomlocke.com>
2022-11-24 10:44:59 +0000

    Example code to config editor with example language

--- site/examples/lang-package/lang-package.js
+++ site/examples/lang-package/lang-package.js
@@ -52,3 +52,13 @@ import {LanguageSupport} from "@codemirror/language"
 export function example() {
   return new LanguageSupport(exampleLanguage, [exampleCompletion])
 }
+
+//!editor
+import {EditorView, basicSetup} from "codemirror"
+
+new EditorView({
+  doc: `(defun check-login (name password) ; absolutely secure\n  (if (equal name "admin")\n    (equal password "12345")\n    #t))`,
+  extensions: [basicSetup, example()],
+  parent: document.querySelector("#editor")
+});
+
